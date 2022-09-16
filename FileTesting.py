78f74ab151a565759ee4a59e73031a1602fc4cd1
Elie-X <eliebouchergendron@gmail.com>
2022-09-16 10:29:39 -0400

    Sync + ajout notes sur Docker

--- FileTesting.py
+++ FileTesting.py
@@ -1,5 +1,6 @@
 import os
-import pathlib
+from pathlib import Path
+
 """
 with open('testfile.txt', 'r') as f:  # On ouvre le fichier en mode Read-Only et on lit ce qui s'y trouve.
     data = f.read()
@@ -26,5 +27,11 @@ for entry in os.listdir(basepath):
     if os.path.isfile(os.path.join(basepath, entry)):
         print(entry)
 """
-
-#Si on choisi
+# https://docs.python.org/3/library/pathlib.html
+basedir = Path("./bidonDir")
+print([x for x in basedir.iterdir() if x.is_dir()])
+# Si on assume que la variable basedir comprend le nom du dossier auquel on veut synchroniser son arbre descendant.
+# Il faudra donc faire une vérification périodique de son arbre descendant et vérifier chaque fichier pour voir s'il
+# a été modifié depuis pour garder seulement la version la plus récente. Il ne sera aussi pas nécessaire de faire
+# une sauvegarde au Cloud si aucun changement n'a été apporté.
+# On fera aussi la vérification pour s'assurer que le ou les Paths mentionnés à synchroniser sont absolus
