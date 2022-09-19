03a17c02027184fd0853a11465d162bdd581ed5f
Elie-X <eliebouchergendron@gmail.com>
2022-09-19 15:54:37 -0400

    début folder_copy

--- folder_copy.py
+++ folder_copy.py
@@ -1,12 +1,19 @@
 # folder_copy.py
 
 from shutil  import copytree
+import datetime
+
+#def syncNow
+#S'assurer qu'on reçoit une Liste de strings suivi d'un destination path
+ct = datetime.datetime.now()
+print(ct)
 
 base = "C:\\Users\\Elie\\Documents\\Projet\\GitHub-Docker\\copyTestBaseFolder"
-destination = "C:\\Users\\Elie\\Documents\\Projet\\GitHub-Docker\\copyTestDestinationFolder\\copyTestBaseFolder"
+destination = "C:\\Users\\Elie\\Documents\\Projet\\GitHub-Docker\\copyTestDestinationFolder\\copyTestBaseFolder" + "backup" + str(ct)
 print(destination)
 
 try :
     copytree(base, destination) #shutil needs a path that doesn't already exist
 except:
+    #print(exception)
     pass
