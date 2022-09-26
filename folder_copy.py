bfb55c918acda497247ecb267a21e68d4908a91f
Elie-X <eliebouchergendron@gmail.com>
2022-09-26 03:24:06 -0400

    Sync + fix folder_copy

--- folder_copy.py
+++ folder_copy.py
@@ -1,19 +1,20 @@
 # folder_copy.py
-
 from shutil  import copytree
 import datetime
 
 #def syncNow
 #S'assurer qu'on reçoit une Liste de strings suivi d'un destination path
-ct = datetime.datetime.now()
-print(ct)
 
+
+#https://docs.python.org/3/library/datetime.html#strftime-and-strptime-behavior
+ct = datetime.datetime.now()
+formatted_ct = ct.strftime("%Y-%m-%d %H-%M-%S.%f")
 base = "C:\\Users\\Elie\\Documents\\Projet\\GitHub-Docker\\copyTestBaseFolder"
-destination = "C:\\Users\\Elie\\Documents\\Projet\\GitHub-Docker\\copyTestDestinationFolder\\copyTestBaseFolder" + "backup" + str(ct)
+destination = f"C:\\Users\\Elie\\Documents\\Projet\\GitHub-Docker\\copyTestDestinationFolder\\copyTestBaseFolder-{formatted_ct}"
 print(destination)
 
 try :
     copytree(base, destination) #shutil needs a path that doesn't already exist
-except:
-    #print(exception)
+except Exception as e:
+    print(e)
     pass
